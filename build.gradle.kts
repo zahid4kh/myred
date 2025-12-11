e5825f31c0694756f503a07c24f55f79566838ca
zahid4kh <halilzahid@gmail.com>
2025-12-11 20:18:15 +0100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,9 +51,9 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
     implementation("io.coil-kt.coil3:coil-network-ktor3:3.0.4")
 
-    implementation("io.github.panpf.sketch4:sketch-compose:4.0.0")
-    implementation("io.github.panpf.sketch4:sketch-http-okhttp:4.0.0")
-    implementation("io.github.panpf.sketch4:sketch-animated-gif:4.0.0")
+    implementation("io.github.panpf.sketch4:sketch-compose:4.4.0-alpha01")
+    implementation("io.github.panpf.sketch4:sketch-http-okhttp:4.4.0-alpha01")
+    implementation("io.github.panpf.sketch4:sketch-animated-gif:4.4.0-alpha01")
 
     implementation("uk.co.caprica:vlcj:4.8.2")
 
