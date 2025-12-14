d356f215f0acef336d09945d17b9df5eb8635bfd
zahid4kh <halilzahid@gmail.com>
2025-12-14 14:08:33 +0100

    added verticall scrollbar

--- src/main/kotlin/ui/screens/RedditScreen.kt
+++ src/main/kotlin/ui/screens/RedditScreen.kt
@@ -1,14 +1,22 @@
 package ui.screens
 
+import androidx.compose.foundation.LocalScrollbarStyle
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.unit.dp
 import com.github.panpf.sketch.PlatformContext
 import ui.components.FullScreenImage
 import ui.components.NextBatchDialog
@@ -41,7 +49,25 @@ fun RedditScreen(
                 onLoadNextBatch = { viewModel.showNextBatchDialog() },
                 onRefreshBatch = { viewModel.refreshCurrentBatch() }
             )
-            TestBatch(viewModel, listState, lazyRowState, children)
+
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                TestBatch(viewModel, listState, lazyRowState, children)
+
+                VerticalScrollbar(
+                    adapter = rememberScrollbarAdapter(listState),
+                    modifier = Modifier
+                        .padding(horizontal = 2.dp, vertical = 10.dp)
+                        .pointerHoverIcon(PointerIcon.Hand)
+                        .align(Alignment.TopEnd),
+                    style = LocalScrollbarStyle.current.copy(
+                        hoverColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        unhoverColor = MaterialTheme.colorScheme.secondary
+                    )
+                )
+            }
+
         }
     }
 
